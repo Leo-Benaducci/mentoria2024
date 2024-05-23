ac8046367c9d81c4145cd617a23458121ded55ad
Gustavo Henrique Braga Fernandes <gutilokossj12@gmail.com>
2024-05-23 18:27:10 -0300

    Testando commit, ajustando somar, subtrair, multiplicar e dividir

--- src/main/java/br/com/lbenaducci/Calculadora.java
+++ src/main/java/br/com/lbenaducci/Calculadora.java
@@ -3,8 +3,24 @@ package br.com.lbenaducci;
 public class Calculadora {
 
     public long somar(int i, int i1) {
+
         return i + i1;
     }
+
+    public long subtrair(int i, int i1) {
+        return i - i1;
+    }
+
+    public long multiplicar(int i, int i1) {
+        return (long) i * i1;
+    }
+
+    public double dividir(int i, int i1) {
+        return (double) i / i1;
+    }
+
+
 }
+
 
 
