59ff798784320f3f5b220b0f94d4aa33058f43ea
Fabiano-Fazan <fazanfabiano86@gmail.com>
2024-05-24 16:31:35 -0300

    segundo teste

--- src/main/java/br/com/lbenaducci/Calculadora.java
+++ src/main/java/br/com/lbenaducci/Calculadora.java
@@ -1,2 +1,44 @@
-package br.com.lbenaducci;public class Calculadora {
+package br.com.lbenaducci;
+
+public class Calculadora {
+    public long somar(long n1, long n2){
+        return n1 + n2;
+    }
+    public long subtrair(long n1, long n2){
+        return n1 - n2;
+    }
+    public long multiplicar(long n1, long n2){
+        return n1 * n2;
+    }
+    public double dividir(double n1, double n2){
+        if(n2==0){
+            throw new IllegalArgumentException("Não da pra dividir 0 com 0 ne");
+        }
+        return n1 / n2;
+    }
+    public double media(double ... num) {
+        double nMedia = 0;
+        for (double num1 : num) {
+            nMedia += num1;
+        }
+        return nMedia / num.length;
+    }
+    public int max(int ... num) {
+        int nMax = 0;
+        for (int num1 : num) {
+            if (num1 > nMax) {
+                nMax =  num1;
+            }
+        }
+        return nMax;
+    }
+    public int min(int ... num) {
+        int nMin = 0;
+        for (int num1 : num) {
+            if (num1 < nMin) {
+                nMin = num1;
+            }
+        }
+        return nMin;
+    }
 }
