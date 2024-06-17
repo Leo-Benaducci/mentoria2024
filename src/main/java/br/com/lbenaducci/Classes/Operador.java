9ad39b2867421c5c533a68184359f45dac5ff9f7
Gustavo Henrique Braga Fernandes <gutilokossj12@gmail.com>
2024-06-17 14:09:00 -0300

    Ajuste teste OperadorTeste

--- src/main/java/br/com/lbenaducci/Classes/Operador.java
+++ src/main/java/br/com/lbenaducci/Classes/Operador.java
@@ -1,5 +1,32 @@
 package br.com.lbenaducci.Classes;
 
 public enum Operador {
+    SOMA{
+        public double calc(double a, double b) {
+            return a + b;
+        }
+    },
 
+    SUBTRACAO{
+        public double calc(double a, double b) {
+            return a - b;
+        }
+
+    },
+
+    MULTIPLICACAO{
+        public double calc(double a, double b) {
+            return a * b;
+        }
+    },
+    DIVISAO{
+        public double calc(double a, double b) {
+            if(b ==0){
+                throw new IllegalArgumentException("Impossível fazer divisão por 0");
+            }
+            return a / b;
+        }
+    };
+
+    public abstract double calc(double a, double b);
 }
