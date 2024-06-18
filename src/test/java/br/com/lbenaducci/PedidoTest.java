964954efd9a5bcc0b4594d01ae88e84958e4628d
Leo Benaducci <leo13benaducci@gmail.com>
2024-06-18 11:05:37 -0300

    Criando testes para o pedido

---
+++ src/test/java/br/com/lbenaducci/PedidoTest.java
@@ -0,0 +1,33 @@
+package br.com.lbenaducci;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PedidoTest {
+	@Test
+	void construtorTest() {
+		List<Item> itens = getItens();
+		Pedido pedido = assertDoesNotThrow(() -> new Pedido(1, itens));
+		assertInstanceOf(Record.class, pedido);
+		assertEquals(1, pedido.mesa());
+		assertEquals(itens, pedido.itens());
+	}
+
+	@Test
+	void valorTotalTest() {
+		List<Item> itens = getItens();
+		Pedido pedido = assertDoesNotThrow(() -> new Pedido(1, itens));
+		assertEquals(21.0, pedido.valorTotal());
+	}
+
+	private List<Item> getItens() {
+		List<Item> itens = new ArrayList<>();
+		itens.add(new Prato("Teste", 10.5, Categoria.ENTRADA, "Item de teste"));
+		itens.add(new Bebida("Teste", 10.5));
+		return itens;
+	}
+}
