813cd2464b0b68c55a84be9c427fbd06f01fc175
Leo Benaducci <leo13benaducci@gmail.com>
2024-06-18 10:24:11 -0300

    Descrevendo o problema a ser resolvido

--- src/main/java/br/com/lbenaducci/Main.java
+++ src/main/java/br/com/lbenaducci/Main.java
@@ -2,6 +2,30 @@ package br.com.lbenaducci;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		/*
+		Crie um sistema de um restaurante:
+		1. Crie um cadastro itens de um cardápio.
+			A base dos itens deve conter:
+				• Nome
+				• Preço
+				• Categoria
+			Crie alguns tipos de itens para o cardápio. Para cada tipo item do cardápio será uma nova classe. Ex:
+				• Bebida (item base)
+				• Prato (item base + descricao)
+				• Combo (item base + lista de itens)
+		2. Crie um cadastro de pedidos de um cliente.
+			Os pedidos não podem ser alterados (record) e devem ter:
+				• Numero de mesa
+				• Lista de itens
+				• Valor total
+		3. Crie um sistema de pagamentos.
+			Com as seguintes opções:
+				• Cartão
+				• Pix
+				• Dinheiro
+			Onde todas as opções devem ter a função pagar que receberá o pedido.
+		Lembre-se de usar enums, classes abstratas e interfaces.
+		Boa sorte!
+		 */
 	}
 }
