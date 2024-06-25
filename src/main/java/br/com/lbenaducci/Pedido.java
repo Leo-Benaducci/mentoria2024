e7b282a0dfb7d73617f3b1a27a1f6c3fb84622dd
Gustavo Henrique Braga Fernandes <gutilokossj12@gmail.com>
2024-06-25 13:09:21 -0300

    Ajustei Record Pedido e atributos

--- src/main/java/br/com/lbenaducci/Pedido.java
+++ src/main/java/br/com/lbenaducci/Pedido.java
@@ -2,21 +2,19 @@ package br.com.lbenaducci;
 
 import java.util.List;
 
-public record Pedido(int numeroMesa, List<Item> itens) {
+public record Pedido(int mesa, List<Item> itens) {
 
-    // Sabe me explicar o que esta fazendo aqui? O que é uma stream? O que o map faz?
+    // Sabe me explicar o que esta fazendo aqui? O que é uma stream? O que o map faz? EXPLICADO
     // Saberia adicionar desconto de 10% em itens do tipo bebida e 5% em itens do tipo sobremesa.
 
     public double valorTotal() {
         // Calcular o valor total somando os preços de todos os itens
         return itens.stream().mapToDouble(Item::getPreco).sum();
     }
-
-    // Poderia ter colocado o nome do atributo de mesa no lugar de criar um metodo que retorna seu atributo com outro nome
-    public int mesa() {
-        return numeroMesa;
-    }
 }
+    // Poderia ter colocado o nome do atributo de mesa no lugar de criar um metodo que retorna seu atributo com outro nome
+    //FEITO, COLOQUEI ALI DEPOIS DE Pedido(int "mesa"...)
+
 
 
 
